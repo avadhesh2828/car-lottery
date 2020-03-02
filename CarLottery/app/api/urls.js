725e8ff16c26271248ffe9f72abe5026578af4ff
ayush gupta <agupta@grepruby.io>
2020-03-02 15:46:05 +0530

    Adding image in home page

--- CarLottery/app/api/urls.js
+++ CarLottery/app/api/urls.js
@@ -6,3 +6,6 @@ export const getSportsUrl = `${getBaseUrl()}/api/v1/sports`;
 export const loginUrl = `${getBaseUrl()}/api/auth/user_login`;
 export const signupUrl = `${getBaseUrl()}/api/auth/signup`;
 export const commonHotLotteriesUrl = `${getBaseUrl()}/api/common/hot_lotteries`;
+
+// Image Urls
+export const contestImgUrl = (imgName) => `${getBaseUrl()}/uploads/contests/${imgName}`;
